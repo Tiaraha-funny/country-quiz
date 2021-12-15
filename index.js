9cdba220322be9c6cbeb32456f6dff21ef67b79a
Tiaraha-funny <peta.jea@onja.org>
2021-12-15 14:23:58 +0300

    added ReactDOMServer.renderToNodeStream('#root');

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactDOMServer from 'react-dom/server';
 import App from "./Components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOMServer.renderToString('#root')
